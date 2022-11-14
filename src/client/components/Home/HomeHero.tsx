362deed66b2fb10f378b5bf8e15cb05e28b10342
hadid <hadidbilla449@gmail.com>
2022-11-15 02:15:44 +0600

    working home hero complete slider

---
+++ src/client/components/Home/HomeHero.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Navigation, Autoplay } from 'swiper';
+import Image from 'next/image';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import 'swiper/css';
+
+function HomeHero() {
+  return (
+    <div className="hero">
+      <div className="hero__slider__area">
+        <Swiper
+          spaceBetween={30}
+          effect="fade"
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          modules={[Navigation, EffectFade, Autoplay]}
+          navigation={true}
+          slidesPerView={1}
+        >
+          <SwiperSlide className="hero__slider">
+            <Image
+              className="slider__img"
+              src="/images/slide1.jpeg"
+              layout="fill"
+              alt={''}
+            />
+            <div className="container hero__slider__content">
+              <h1 className="hero__title">Comprehensive weather imagery</h1>
+              <p className="hero__subtitle">
+                This is not your ordinary static imagery
+              </p>
+              <button className="btn">Learn More</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="hero__slider">
+            <Image
+              className="slider__img"
+              src="/images/slider2.jpeg"
+              layout="fill"
+              alt={''}
+            />
+            <div className="container hero__slider__content">
+              <h1 className="hero__title">Comprehensive weather imagery</h1>
+              <p className="hero__subtitle">
+                This is not your ordinary static imagery
+              </p>
+              <button className="btn">Learn More</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="hero__slider">
+            <Image
+              className="slider__img"
+              src="/images/slider3.jpeg"
+              layout="fill"
+              alt={''}
+            />
+            <div className="container hero__slider__content">
+              <h1 className="hero__title">Comprehensive weather imagery</h1>
+              <p className="hero__subtitle">
+                This is not your ordinary static imagery
+              </p>
+              <button className="btn">Learn More</button>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default HomeHero;
