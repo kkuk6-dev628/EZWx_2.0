ff7aaf362844ee8a3f7ac918b8a6639065db7a34
Hadid Billa Hridoy <69754874+hadidbilla@users.noreply.github.com>
2022-11-14 23:31:04 +0600

    Hadid design header (#31)
    * working on header
    
    * complete header

--- src/client/components/layout/Header.tsx
+++ src/client/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { SvgDropDown } from '../utils/SvgIcons';
 
 export default function Header() {
   const links = [
@@ -54,57 +55,68 @@ export default function Header() {
   ];
   return (
     <div className="header">
-      <div className="header__lft">
-        <Link href="/home">
-          <div className="header__img__area">
-            <Image
-              src="/images/Logo.png"
-              width={100}
-              height={100}
-              alt="logo"
-              objectFit="contain"
-              className="header__img"
-            />
+      <div className="container">
+        <div className="header__wrp">
+          <div className="header__lft">
+            <Link href="/home">
+              <Image
+                src="/images/Logo.png"
+                layout="fill"
+                alt="logo"
+                objectFit="contain"
+                className="header__img"
+              />
+            </Link>
           </div>
-        </Link>
-      </div>
-      <div className="header__mid">
-        <ul className="header__nav">
-          {links.map((link) => (
-            <li key={link.id} className="header__nav__item">
-              <Link href={link.link}>
-                <span className="header__nav__link">{link.name}</span>
+          <div className="header__mid">
+            <ul className="header__nav">
+              {links.map((link) => (
+                <li key={link.id} className="header__nav__item">
+                  <Link className="header__nav__anc" href={link.link}>
+                    <span className="header__nav__link">{link.name}</span>
+                    <div className="header__nav__icon">
+                      {link.children && <SvgDropDown />}
+                    </div>
+                  </Link>
+                  {link.children && (
+                    <div className="header__nav__dropdown">
+                      <ul className="header__nav__dropdown__list flex flex-column">
+                        {link.children.map((child) => (
+                          <li
+                            key={child.id}
+                            className="header__nav__dropdown__item"
+                          >
+                            <Link
+                              className="header__nav__dropdown__anc"
+                              href={child.link}
+                            >
+                              <span className="header__nav__dropdown__link">
+                                {child.name}
+                              </span>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="header__rgt">
+            <div className="header__rgt__btns flex flex-align-center">
+              <Link href="/login">
+                <button className="header__rgt__btn header__rgt__btn--lft btn">
+                  New to EZWxBrief?
+                </button>
               </Link>
-              {link.children && (
-                <div className="header__nav__dropdown">
-                  <ul className="header__nav__dropdown__list">
-                    {link.children.map((child) => (
-                      <li
-                        key={child.id}
-                        className="header__nav__dropdown__item"
-                      >
-                        <Link href={child.link}>
-                          <span className="header__nav__dropdown__link">
-                            {child.name}
-                          </span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="header__rgt">
-        <div className="header__rgt__btns">
-          <Link href="/login">
-            <button className="header__rgt__btn">New to EZWxBrief?</button>
-          </Link>
-          <Link href="/signup">
-            <button className="header__rgt__btn">Join Now | Sign in</button>
-          </Link>
+              <Link href="/signup">
+                <button className="header__rgt__btn header__rgt__btn--rgt btn">
+                  Join Now | Sign in
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
