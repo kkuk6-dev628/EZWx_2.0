948fb45dca31ff5f2a730a07e5a476b002372e83
hadid <hadidbilla449@gmail.com>
2022-11-18 01:38:52 +0600

    complete responsive menu

--- src/client/components/Home/HomeHero.tsx
+++ src/client/components/Home/HomeHero.tsx
@@ -3,16 +3,14 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper';
 import Image from 'next/image';
 import 'swiper/css/navigation';
-import 'swiper/css/effect-fade';
 import 'swiper/css';
-
+const css = { maxWidth: '100%', height: 'auto' };
 function HomeHero() {
   return (
     <div className="hero">
       <div className="hero__slider__area">
         <Swiper
-          spaceBetween={30}
-          effect="fade"
+          loop={true}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
           modules={[Navigation, Autoplay]}
           navigation={true}
@@ -30,7 +28,7 @@ function HomeHero() {
               <p className="hero__subtitle">
                 This is not your ordinary static imagery
               </p>
-              <button className="btn">Learn More</button>
+              <button className="btn btn--primary">Learn More</button>
             </div>
           </SwiperSlide>
           <SwiperSlide className="hero__slider">
@@ -45,7 +43,7 @@ function HomeHero() {
               <p className="hero__subtitle">
                 This is not your ordinary static imagery
               </p>
-              <button className="btn">Learn More</button>
+              <button className="btn btn--primary">Learn More</button>
             </div>
           </SwiperSlide>
           <SwiperSlide className="hero__slider">
@@ -60,26 +58,10 @@ function HomeHero() {
               <p className="hero__subtitle">
                 This is not your ordinary static imagery
               </p>
-              <button className="btn">Learn More</button>
+              <button className="btn btn--primary">Learn More</button>
             </div>
           </SwiperSlide>
         </Swiper>
-      </div>
-      <div className="hero__card__area">
-        <div className="hero__card__area__header">
-          <h1 className="hero__card__area__title"></h1>
-          <h1 className="hero__card__area__text text"></h1>
-        </div>
-        <div className="hero__card__container">
-          <div className="hero__card">
-            <div className="hero__card__top"></div>
-            <div className="hero__card__btm">
-              <div className="hero__card__icon"></div>
-              <h2 className="hero__card__title"></h2>
-              <p className="hero__card__text text"></p>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
