217d8ed159e63b6bd90e357b4a51ebc5c5063452
hadid <hadidbilla449@gmail.com>
2022-11-26 21:52:11 +0600

    fix map page dropdown issue

--- src/client/components/shared/MapTabs.tsx
+++ src/client/components/shared/MapTabs.tsx
@@ -1,10 +1,12 @@
-import React, { FormEventHandler } from 'react';
+import React, { FormEventHandler, useState } from 'react';
 import {
+  SvgAir,
   SvgLayer,
   SvgMap,
   SvgProfileCharge,
   SvgRoute,
   SvgTemperature,
+  SvgThreeDot,
 } from '../utils/SvgIcons';
 
 interface MapTabsProps {
@@ -20,29 +22,52 @@ function MapTabs({
   profileClick,
   basemapClick,
 }: MapTabsProps) {
+  const [showDropDown, setShowDropDown] = useState(false);
   return (
     <div className="tabs">
       <button className="tabs__btn">1040Z</button>
       <button className="tabs__btn" onClick={layerClick}>
         <SvgLayer />
-        <p className="text btn__text">Layer</p>
+        <p className="btn__text">Layer</p>
       </button>
       <button className="tabs__btn" onClick={routeClick}>
         <SvgRoute />
-        <p className="text btn__text">Route</p>
+        <p className="btn__text">Route</p>
       </button>
       <button className="tabs__btn" onClick={profileClick}>
         <SvgProfileCharge />
-        <p className="text btn__text">Profile</p>
+        <p className="btn__text">Profile</p>
       </button>
-      <button className="tabs__btn" onClick={basemapClick}>
+      <button
+        className="tabs__btn tabs__btn--more"
+        onClick={() => setShowDropDown(!showDropDown)}
+      >
+        <SvgThreeDot />
+      </button>
+      <button className="tabs__btn tabs__btn--hide" onClick={basemapClick}>
         <SvgMap />
-        <p className="text btn__text">Base map</p>
+        <p className="btn__text">Base map</p>
       </button>
-      <button className="tabs__btn">
+      <button className="tabs__btn tabs__btn--hide">
         <SvgTemperature />
-        <p className="text btn__text">7 days</p>
+        <p className="btn__text">7 days</p>
       </button>
+      {showDropDown && (
+        <div className="tabs__dropdown">
+          <button className=" tabs__btn--dropdown">
+            <SvgAir />
+            <p className="btn__text">Airport</p>
+          </button>
+          <button className="tabs__btn--dropdown" onClick={basemapClick}>
+            <SvgMap />
+            <p className=" btn__text">Base map</p>
+          </button>
+          <button className=" tabs__btn--dropdown">
+            <SvgTemperature />
+            <p className="btn__text">7 days</p>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
