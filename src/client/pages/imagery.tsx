0baa48f89c4a232934e7f4c669deb9762a9c8fa8
hadid <hadidbilla449@gmail.com>
2022-12-01 02:13:04 +0600

    complete responsive

--- src/client/pages/imagery.tsx
+++ src/client/pages/imagery.tsx
@@ -3,8 +3,15 @@ import { IoMdArrowDropdown } from 'react-icons/io';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { GrFormClose } from 'react-icons/gr';
 import { FaPlayCircle } from 'react-icons/fa';
+import { BsBookmarkPlus, BsShare } from 'react-icons/bs';
+import { MdOutlineSaveAlt } from 'react-icons/md';
 import MapTabs from '../components/shared/MapTabs';
-import { SvgDropDown } from '../components/utils/SvgIcons';
+import {
+  SvgDropDown,
+  SvgRefresh,
+  SvgTabs,
+  SvgZoom,
+} from '../components/utils/SvgIcons';
 import Map from '../components/map/leaflet';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
@@ -56,17 +63,80 @@ function Imagery() {
           <h2 className="igry__range__title">
             Valid 0000Z Feb 27 to 1200Z Feb 27
           </h2>
-          <div className="igry__range__wrp container">
-            <button className="igry__range__play__btn">
-              <FaPlayCircle className="igry__range__play--icon" />
-            </button>
-            <Slider
-              min={20}
-              defaultValue={20}
-              marks={marks}
-              step={null}
-              className="igry__range__slider"
-            />
+          <div className=" container">
+            <div className="igry__range__wrp">
+              <div className="igry__btn__area">
+                <button className="igry__range__play__btn">
+                  <FaPlayCircle className="igry__range__play--icon" />
+                </button>
+              </div>
+              <div className="igry__slider__area">
+                <Slider
+                  min={20}
+                  defaultValue={20}
+                  marks={marks}
+                  step={null}
+                  className="igry__range__slider"
+                />
+                <div className="igry__marker__area">
+                  <p className="igry__range__txt">
+                    1200Z Feb 27 - 0000Z Feb 28
+                  </p>
+                  <p className="igry__range__txt">
+                    1200Z Feb 27 - 0000Z Feb 28
+                  </p>
+                  <p className="igry__range__txt">
+                    1200Z Feb 27 - 0000Z Feb 28
+                  </p>
+                  <p className="igry__range__txt">
+                    1200Z Feb 27 - 0000Z Feb 28
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="igry__tbs">
+              <div className="igry__tbs__btn__area">
+                <button className="igry__tbs__btn">
+                  <BsBookmarkPlus className="igry__tbs__btn--icon" />
+                  <p className="igry__tbs__txt">Save</p>
+                </button>
+              </div>
+              <div className="igry__tbs__btn__area">
+                <button className="igry__tbs__btn">
+                  <MdOutlineSaveAlt className="igry__tbs__btn--icon" />
+                  <p className="igry__tbs__txt">Export</p>
+                </button>
+              </div>
+              <div className="igry__tbs__btn__area">
+                <button className="igry__tbs__btn">
+                  <BsShare className="igry__tbs__btn--icon" />
+                  <p className="igry__tbs__txt">Share</p>
+                </button>
+              </div>
+              <div className="igry__tbs__btn__area">
+                <button className="igry__range__play__btn">
+                  <FaPlayCircle className="igry__range__play--icon" />
+                </button>
+              </div>
+              <div className="igry__tbs__btn__area">
+                <button className="igry__tbs__btn">
+                  <SvgTabs />
+                  <p className="igry__tbs__txt">Future</p>
+                </button>
+              </div>
+              <div className="igry__tbs__btn__area">
+                <button className="igry__tbs__btn">
+                  <SvgZoom />
+                  <p className="igry__tbs__txt">Expand</p>
+                </button>
+              </div>
+              <div className="igry__tbs__btn__area">
+                <button className="igry__tbs__btn">
+                  <SvgRefresh />
+                  <p className="igry__tbs__txt">Expand</p>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
