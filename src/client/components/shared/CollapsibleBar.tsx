3975428060d0d3046f981db275f220672645c550
hadid <hadidbilla449@gmail.com>
2022-12-09 09:01:38 +0600

    complete tooltip

--- src/client/components/shared/CollapsibleBar.tsx
+++ src/client/components/shared/CollapsibleBar.tsx
@@ -3,62 +3,23 @@ import { FaChevronDown } from 'react-icons/fa';
 import { DateObject } from 'react-multi-date-picker';
 import TimeSlider from './TimeSlider';
 
+interface DateData {
+  month: number;
+  day: string;
+  hour: number;
+}
+
 function CollapsibleBar({
   handleTime,
 }: {
-  handleTime: (time: string) => void;
+  handleTime: (time: DateData) => void;
 }) {
-  const [isColleps, setIsColleps] = useState(false);
-  const [date, _] = useState(new DateObject());
-
-  const getTime = (key, value) => {
-    if (key === 'hour' && value < 0) {
-      const newDate = new DateObject(date);
-      newDate[key] += value;
-      return `${newDate.weekDay.shortName} ${newDate.month.index}`;
-    }
-    if (key === 'hour' && value > 0) {
-      const newDate = new DateObject(date);
-      newDate[key] += value;
-      return `${newDate.weekDay.shortName} ${newDate.month.index}`;
-    }
-  };
-
   // const [windowSize, setWindowSize] = useState(setWindowSize());
 
   return (
     <div className="collps">
       <div className="collps__wrp">
-        <div className="collps__top__area">
-          <div className="collps__btn__area">
-            <button
-              onClick={() => setIsColleps(!isColleps)}
-              className="collps__btn"
-            >
-              {date.day > 10 ? `${date.day}Z` : `0${date.day}Z`}
-              <FaChevronDown className="collps__icon" />
-            </button>
-          </div>
-          <div className="collps__dot__area">
-            <TimeSlider handleTime={handleTime} isColleps={isColleps} />
-          </div>
-        </div>
-        <div className="collps__btm__area">
-          <div className="collps__date__area">
-            <p className="collps__date__text">{getTime('hour', -12)}</p>
-          </div>
-          <div className="collps__date__area">
-            <p className="collps__date__text">
-              {date.weekDay.shortName} {date.month.index}
-            </p>
-          </div>
-          <div className="collps__date__area">
-            <p className="collps__date__text">{getTime('hour', 24)}</p>
-          </div>
-          <div className="collps__date__area">
-            <p className="collps__date__text">{getTime('hour', 42)}</p>
-          </div>
-        </div>
+        <TimeSlider handleTime={handleTime} />
       </div>
     </div>
   );
--- src/client/components/shared/TimeSlider.tsx
+++ src/client/components/shared/TimeSlider.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
 import { DateObject } from 'react-multi-date-picker';
-function TimeSlider({
-  isColleps,
-  handleTime,
-}: {
-  handleTime: (time: string) => void;
-  isColleps: boolean;
-}) {
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+interface DateData {
+  month: number;
+  day: string;
+  hour: number;
+}
+
+function TimeSlider({ handleTime }: { handleTime: (time: DateData) => void }) {
   const [amountOfData, setAmountOfData] = useState(74);
-  const [date, _] = useState(new DateObject());
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [date, setDate] = useState(new DateObject());
+  const [isColleps, setIsColleps] = useState(false);
   const randomColorData = ['red', 'green', 'yellow'];
   useEffect(() => {
     function handleWindowResize() {
@@ -31,49 +36,130 @@ function TimeSlider({
     if (key === 'hour' && value < 0) {
       const newDate = new DateObject(date);
       newDate[key] += value;
-      return `${newDate.weekDay.shortName} ${newDate.hour}`;
+      const dateData = {
+        month: newDate.month.index,
+        day: newDate.weekDay.shortName,
+        hour: newDate.hour,
+      };
+      return dateData;
     }
     if (key === 'hour' && value > 0) {
       const newDate = new DateObject(date);
       newDate[key] += value;
-      return `${newDate.weekDay.shortName} ${newDate.hour}`;
+      const dateData = {
+        month: newDate.month.index,
+        day: newDate.weekDay.shortName,
+        hour: newDate.hour,
+      };
+      return dateData;
     }
+  };
+
+  const hnadelSetDate = (index, limit) => {
+    console.log(index);
+    handleTime(getTime('hour', index - limit));
+    setSelectedDate(getTime('hour', index - limit));
+    //setdate to selected date
+    const newDate = new DateObject(date);
+    newDate.hour += index - limit;
+    setDate(newDate);
   };
 
   return (
     <>
-      {[...Array(12)].map((_, index) => (
-        //get
-        <button
-          onClick={() => handleTime(getTime('hour', index - 12))}
-          key={index}
-          className={`collps__dot__btn ${randomColorData[index % 4]} ${
-            isColleps ? 'collps__dot__btn--full' : ''
-          }`}
-        >
-          {[...Array(12)].map((_, i) => (
-            <span key={i} className="collps__dot">
-              &nbsp;
-            </span>
+      <div className="collps__top__area">
+        <div className="collps__btn__area">
+          <button
+            onClick={() => setIsColleps(!isColleps)}
+            className="collps__btn"
+          >
+            {selectedDate
+              ? selectedDate.hour > 10
+                ? `${selectedDate.hour}Z`
+                : `0${selectedDate.hour}Z`
+              : date.hour > 10
+              ? `${date.hour}Z`
+              : `0${date.hour}Z`}
+            <FaChevronDown className="collps__icon" />
+          </button>
+        </div>
+        <div className="collps__dot__area">
+          {[...Array(12)].map((_, index) => (
+            //get
+            <Tippy
+              content={
+                <span>
+                  {getTime('hour', index - 12).day +
+                    ' ' +
+                    getTime('hour', index - 12).hour}
+                </span>
+              }
+            >
+              <button
+                onClick={() => hnadelSetDate(index, 12)}
+                key={index}
+                className={`collps__dot__btn ${randomColorData[index % 4]} ${
+                  isColleps ? 'collps__dot__btn--full' : ''
+                }`}
+              >
+                {[...Array(12)].map((_, i) => (
+                  <span key={i} className="collps__dot">
+                    &nbsp;
+                  </span>
+                ))}
+              </button>
+            </Tippy>
           ))}
-        </button>
-      ))}
-      {[...Array(amountOfData)].map((_, index) => (
-        //get
-        <button
-          key={index}
-          onClick={() => handleTime(getTime('hour', index))}
-          className={`collps__dot__btn ${randomColorData[index % 4]} ${
-            isColleps ? 'collps__dot__btn--full' : ''
-          }`}
-        >
-          {[...Array(12)].map((_, i) => (
-            <span key={i} className="collps__dot">
-              &nbsp;
-            </span>
+          {[...Array(amountOfData)].map((_, index) => (
+            //get
+            <Tippy
+              content={
+                <span>
+                  {getTime('hour', index)?.day +
+                    ' ' +
+                    getTime('hour', index)?.hour}
+                </span>
+              }
+            >
+              <button
+                key={index}
+                onClick={() => hnadelSetDate(index, 0)}
+                className={`collps__dot__btn ${randomColorData[index % 4]} ${
+                  isColleps ? 'collps__dot__btn--full' : ''
+                }`}
+              >
+                {[...Array(12)].map((_, i) => (
+                  <span key={i} className="collps__dot">
+                    &nbsp;
+                  </span>
+                ))}
+              </button>
+            </Tippy>
           ))}
-        </button>
-      ))}
+        </div>
+      </div>
+      <div className="collps__btm__area">
+        <div className="collps__date__area">
+          <p className="collps__date__text">
+            {getTime('hour', -12).day + ' ' + getTime('hour', -12).hour}
+          </p>
+        </div>
+        <div className="collps__date__area">
+          <p className="collps__date__text">
+            {date.weekDay.shortName} {date.month.index}
+          </p>
+        </div>
+        <div className="collps__date__area">
+          <p className="collps__date__text">
+            {getTime('hour', 24).day + ' ' + getTime('hour', 24).hour}
+          </p>
+        </div>
+        <div className="collps__date__area">
+          <p className="collps__date__text">
+            {getTime('hour', 42).day + ' ' + getTime('hour', 42).hour}
+          </p>
+        </div>
+      </div>
     </>
   );
 }
