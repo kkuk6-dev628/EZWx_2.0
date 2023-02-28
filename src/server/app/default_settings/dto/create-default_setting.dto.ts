5eaf7f3e7638a8ac5db9855f9456cd3701e3f3c8
Muhammad Hamza Haneef Qureshi <muhammadhamzahaneef@gmail.com>
2023-02-28 13:28:15 +0500

    default settings dto fix

--- src/server/app/default_settings/dto/create-default_setting.dto.ts
+++ src/server/app/default_settings/dto/create-default_setting.dto.ts
@@ -1,10 +1,10 @@
 export class CreateDefaultSettingDto {
   default_home_airport: string;
-  default_temperature_unit: string;
-  default_time_display_unit: string;
-  default_wind_speed_unit: string;
-  default_distance_unit: string;
-  default_visibility_unit: string;
+  default_temperature_unit: boolean;
+  default_time_display_unit: boolean;
+  default_wind_speed_unit: boolean;
+  default_distance_unit: boolean;
+  default_visibility_unit: boolean;
 
   //AirCraft Setting Fields
   max_takeoff_weight_category: string;
@@ -27,8 +27,11 @@ export class CreateDefaultSettingDto {
   surface_visibility_along_route_min: number;
   surface_visibility_along_route_max: number;
 
-  en_route_icing_probability_min: string;
-  en_route_icing_probability_max: string;
+  en_route_icing_probability_min: number;
+  en_route_icing_probability_max: number;
+
+  en_route_icing_intensity_min: number;
+  en_route_icing_intensity_max: number;
 
   en_route_turbulence_intensity_min: number;
 
