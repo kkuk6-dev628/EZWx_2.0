cc1362b9e06b2d5ada607405772f6391a2054315
Kang <kkuk628@outlook.com>
2023-03-31 09:57:50 +0800

    create https server in nestjs

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 
 import { ServerModule } from 'src/server/server.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ServerModule);
+  const httpsOptions = {
+    key: process.env.PKEY_PATH ? fs.readFileSync(process.env.PKEY_PATH) : undefined,
+    cert: process.env.CERT_PATH ? fs.readFileSync(process.env.CERT_PATH) : undefined,
+  };
+  const app = httpsOptions.key
+    ? await NestFactory.create(ServerModule, {
+        httpsOptions,
+      })
+    : await NestFactory.create(ServerModule);
   app.use(cookieParser());
 
   await app.listen(process.env.PORT || 3000);
